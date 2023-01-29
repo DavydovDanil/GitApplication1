16d1816df8c2e5fc0c37f118dc46e181a51b46aa
ТАТЫЭНА <tshatkov4@gmail.com>
2023-01-29 18:27:04 +0300

    добавлен MatrixB

--- GitApplication1/Program.cs
+++ GitApplication1/Program.cs
@@ -7,12 +7,11 @@
         static void Main(string[] args)
         {
 
-
         }
-        public void MatrixA(int n, int m)
+        public void MatrixB(int n, int m)
         {
+            Console.ForegroundColor = ConsoleColor.Magenta;
             int[,] mass = new int[n, m];
-
             for (int i = 0; i < n; ++i)
             {
                 for (int j = 0; j < m - 1; ++j)
@@ -21,7 +20,6 @@
                         mass[i, j + 1] = i + j;
                 }
             }
-
             for (int i = 0; i < n; ++i, Console.WriteLine())
             {
                 for (int j = 0; j < m; ++j)
@@ -30,6 +28,5 @@
                 }
             }
         }
-
     }
 }
