06dfaaf5d2e209a2f1db95ee4da7fedb7c058258
Данил Давыдов <dan776@bk.ru>
2023-01-29 18:24:19 +0300

    Add project files.

---
+++ GitApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FcknGypsies
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+        public void MatrixA(int n, int m)
+        {
+            int[,] mass = new int[n, m];
+
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = 0; j < m - 1; ++j)
+                {
+                    if (i == j)
+                        mass[i, j + 1] = i + j;
+                }
+            }
+
+            for (int i = 0; i < n; ++i, Console.WriteLine())
+            {
+                for (int j = 0; j < m; ++j)
+                {
+                    Console.Write(mass[i, j] + " ");
+                }
+            }
+        }
+
+    }
+}
